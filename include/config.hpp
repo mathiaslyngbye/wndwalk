a6a01538cdbfc00b1d97a526603f7ed257b10a06
Mathias Lyngbye <mathias@allesen.dk>
2025-06-07 21:42:55 +0200

    Create simple spawn detached function

--- include/config.hpp
+++ include/config.hpp
@@ -5,13 +5,44 @@
 // Modifier macro
 #define MOD_KEY MOD_ALT
 
-// Action function
 void action(const Arg &arg) {
     int index = std::get<int>(arg);
     wchar_t msg[64];
     swprintf_s(msg, L"Hotkey %d pressed!", index);
     MessageBox(NULL, msg, L"Hotkey Triggered", MB_OK);
-};
+}
+
+void spawn(const Arg& arg)
+{
+    // Get command
+    const wchar_t* argCommand = std::get<const wchar_t*>(arg);
+    std::wstring command(argCommand);
+
+    // Get user directory
+    wchar_t userDir[MAX_PATH] = {};
+    GetEnvironmentVariableW(L"USERPROFILE", userDir, MAX_PATH);
+
+    // Get process info
+    STARTUPINFOW si = {};
+    PROCESS_INFORMATION pi = {};
+    si.cb = sizeof(si);
+
+    CreateProcessW(
+        NULL,               // Application name (NULL = use command line)
+        command.data(),     // Command line (mutable!)
+        NULL,               // Process security attributes
+        NULL,               // Thread security attributes
+        FALSE,              // Inherit handles
+        CREATE_NEW_CONSOLE, // Flags: new console, detached
+        NULL,               // Use parent's environment
+        userDir,            // Use parent's current directory
+        &si,                // Startup info
+        &pi                 // Process info
+    );
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+}
 
 // Hotkey table
 const std::vector<Key> keys = {
@@ -31,5 +62,5 @@ const std::vector<Key> keys = {
     {MOD_KEY,               'M',        action,     0},
     {MOD_KEY | MOD_SHIFT,   'Q',        action,     0},
     {MOD_KEY | MOD_SHIFT,   'C',        action,     0},
-    {MOD_KEY | MOD_SHIFT,   VK_RETURN,  action,     0},
+    {MOD_KEY | MOD_SHIFT,   VK_RETURN,  spawn,      L"wt.exe"},
 };
